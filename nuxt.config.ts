a37b523748eecedc66df42318d0ebb5c0bb0b6a4
milley69 <69milley@gmail.com>
2023-11-21 22:37:39 +0500

    `add full pwa 🐻‍❄️`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/pwa/apple-touch-icon.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/pwa/ios/32.png' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/pwa/ios/16.png' },
-        { rel: 'manifest', href: '/pwa/manifest.json' },
+        { rel: 'manifest', href: '/pwa/site.webmanifest' },
       ],
       meta: [
         /* PWA */
